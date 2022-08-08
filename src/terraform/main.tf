1738f39806cfaec3288a991e44d5d4b9c0146bd4
Amarender <dodleamarender.reddy@gmail.com>
2022-08-08 10:23:30 +1000

    Stage Added

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -22,8 +22,8 @@ resource "azurerm_resource_group" "example" {
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "VNET01" {
+  name                = "VNET01"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
